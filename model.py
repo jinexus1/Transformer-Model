9b3a2bb74adad27409f9b684d7c8436a47d8b360
jinexus1 <ap2929470@gmail.com>
2024-11-07 22:58:52 +0530

    rename commit

--- model.py
+++ model.py
@@ -24,4 +24,9 @@ class PositionalEncoding(nn.Module):
         # positional Encoding
         pe=torch.zeros(seq_len, d_model)
         #Create a vector of shape
-        position=torch.arrange(0,seq_len, dtype=torch.float).unsqueeze(1)
+        position=torch.arange(0,seq_len, dtype=torch.float).unsqueeze(1)
+        div_term=torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0)/d_model))
+        #Applying sin for even positions
+        pe[:,0::2]=torch.sin(position*div_term)
+        #Applying cosin for odd positions
+        pe[:,1::2]=torch.cos(position*div_term)
