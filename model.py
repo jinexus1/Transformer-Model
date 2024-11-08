e9213182b31d18dff577b6e3dcc1c9df37dd5549
jinexus1 <ap2929470@gmail.com>
2024-11-08 10:33:30 +0530

    Complete Model

--- model.py
+++ model.py
@@ -1,65 +1,85 @@
-import numpy as np
 import torch
 import torch.nn as nn
 import math
 
-class InputEmbeddings(nn.Module):
-    def __init__(self,d_model:int,vocab_size:int):
-        super().__init__()
-        self.d_model=d_model
-        self.vocab_size=vocab_size
-        self.embedding=nn.Embedding(vocab_size,d_model)
-
-    def forward(self,x):
-        return self.embedding(x)*math.sqrt(self.d_model)
-
-class PositionalEncoding(nn.Module):
-    # Dropout is used to prevent overfitting
-    def __init__(self, d_model:int,seq_len:int, dropout=float)->None:
-        super().__init__()
-        self.dropout=nn.Dropout(dropout)
-        self.d_model=d_model
-        self.seq_len=seq_len
-        # Create a matrix of shape (seq_len, d_model)
-        # positional Encoding
-        pe=torch.zeros(seq_len, d_model)
-        #Create a vector of shape
-        position=torch.arange(0,seq_len, dtype=torch.float).unsqueeze(1)
-        div_term=torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0)/d_model))
-        #Applying sin for even positions
-        pe[:,0::2]=torch.sin(position*div_term)
-        #Applying cosin for odd positions
-        pe[:,1::2]=torch.cos(position*div_term)
-
-        pe=pe.unsqueeze(0)#make a tensor of size (1,seq_len,d_model)
-        #to save tensor with the file
-        self.register_buffer('pe',pe)
-    def forward(self,x):
-        x=x+(self.pe[:,:x.shape[1],:]).requires_grad_(False)
-        return self.dropout(x)
-    
 class LayerNormalization(nn.Module):
-    def __init__(self, eps:float=10**-6)->None:
+
+    def __init__(self, features: int, eps:float=10**-6) -> None:
         super().__init__()
-        self.eps=eps 
-        self.alpha=nn.Parameter(torch.ones(1))#Multiplied
-        self.bias=nn.Parameter(torch.zeros(1))#Added
-    
-    def forward(self,x):
-        mean=x.mean(dim=-1,keepdim=True)
-        std=x.std(dim=-1,keepdim=True)
-        return self.alpha*(x-mean)/(std+self.eps)+self.bias
+        self.eps = eps
+        self.alpha = nn.Parameter(torch.ones(features)) # alpha is a learnable parameter
+        self.bias = nn.Parameter(torch.zeros(features)) # bias is a learnable parameter
+
+    def forward(self, x):
+        # x: (batch, seq_len, hidden_size)
+         # Keep the dimension for broadcasting
+        mean = x.mean(dim = -1, keepdim = True) # (batch, seq_len, 1)
+        # Keep the dimension for broadcasting
+        std = x.std(dim = -1, keepdim = True) # (batch, seq_len, 1)
+        # eps is to prevent dividing by zero or when std is very small
+        return self.alpha * (x - mean) / (std + self.eps) + self.bias
 
 class FeedForwardBlock(nn.Module):
-    def __init__(self,d_model:int,d_ff)->None:
+
+    def __init__(self, d_model: int, d_ff: int, dropout: float) -> None:
         super().__init__()
-        self.linear_1=nn.Linear(d_model, d_ff)#W1  and B1
-        self.dropout=nn.Dropout(dropout)
-        self.linear_2=nn.Linear(d_ff,d_model)#W2 and B2
-    # (batch, seq_len, d_model) --> (batch, seq_len, h, d_k) --> (batch, h, seq_len, d_k)
-    def forward(self,x):
+        self.linear_1 = nn.Linear(d_model, d_ff) # w1 and b1
+        self.dropout = nn.Dropout(dropout)
+        self.linear_2 = nn.Linear(d_ff, d_model) # w2 and b2
+
+    def forward(self, x):
+        # (batch, seq_len, d_model) --> (batch, seq_len, d_ff) --> (batch, seq_len, d_model)
         return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
+
+class InputEmbeddings(nn.Module):
+
+    def __init__(self, d_model: int, vocab_size: int) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.vocab_size = vocab_size
+        self.embedding = nn.Embedding(vocab_size, d_model)
+
+    def forward(self, x):
+        # (batch, seq_len) --> (batch, seq_len, d_model)
+        # Multiply by sqrt(d_model) to scale the embeddings according to the paper
+        return self.embedding(x) * math.sqrt(self.d_model)
     
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, d_model: int, seq_len: int, dropout: float) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.seq_len = seq_len
+        self.dropout = nn.Dropout(dropout)
+        # Create a matrix of shape (seq_len, d_model)
+        pe = torch.zeros(seq_len, d_model)
+        # Create a vector of shape (seq_len)
+        position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1) # (seq_len, 1)
+        # Create a vector of shape (d_model)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model)) # (d_model / 2)
+        # Apply sine to even indices
+        pe[:, 0::2] = torch.sin(position * div_term) # sin(position * (10000 ** (2i / d_model))
+        # Apply cosine to odd indices
+        pe[:, 1::2] = torch.cos(position * div_term) # cos(position * (10000 ** (2i / d_model))
+        # Add a batch dimension to the positional encoding
+        pe = pe.unsqueeze(0) # (1, seq_len, d_model)
+        # Register the positional encoding as a buffer
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + (self.pe[:, :x.shape[1], :]).requires_grad_(False) # (batch, seq_len, d_model)
+        return self.dropout(x)
+
+class ResidualConnection(nn.Module):
+    
+        def __init__(self, features: int, dropout: float) -> None:
+            super().__init__()
+            self.dropout = nn.Dropout(dropout)
+            self.norm = LayerNormalization(features)
+    
+        def forward(self, x, sublayer):
+            return x + self.dropout(sublayer(self.norm(x)))
+
 class MultiHeadAttentionBlock(nn.Module):
 
     def __init__(self, d_model: int, h: int, dropout: float) -> None:
@@ -113,16 +133,6 @@ class MultiHeadAttentionBlock(nn.Module):
         # (batch, seq_len, d_model) --> (batch, seq_len, d_model)  
         return self.w_o(x)
 
-class ResidualConnection(nn.Module):
-    
-        def __init__(self, features: int, dropout: float) -> None:
-            super().__init__()
-            self.dropout = nn.Dropout(dropout)
-            self.norm = LayerNormalization(features)
-    
-        def forward(self, x, sublayer):
-            return x + self.dropout(sublayer(self.norm(x)))
-        
 class EncoderBlock(nn.Module):
 
     def __init__(self, features: int, self_attention_block: MultiHeadAttentionBlock, feed_forward_block: FeedForwardBlock, dropout: float) -> None:
@@ -135,7 +145,7 @@ class EncoderBlock(nn.Module):
         x = self.residual_connections[0](x, lambda x: self.self_attention_block(x, x, x, src_mask))
         x = self.residual_connections[1](x, self.feed_forward_block)
         return x
-
+    
 class Encoder(nn.Module):
 
     def __init__(self, features: int, layers: nn.ModuleList) -> None:
@@ -147,7 +157,7 @@ class Encoder(nn.Module):
         for layer in self.layers:
             x = layer(x, mask)
         return self.norm(x)
-    
+
 class DecoderBlock(nn.Module):
 
     def __init__(self, features: int, self_attention_block: MultiHeadAttentionBlock, cross_attention_block: MultiHeadAttentionBlock, feed_forward_block: FeedForwardBlock, dropout: float) -> None:
@@ -184,7 +194,7 @@ class ProjectionLayer(nn.Module):
     def forward(self, x) -> None:
         # (batch, seq_len, d_model) --> (batch, seq_len, vocab_size)
         return self.proj(x)
-
+    
 class Transformer(nn.Module):
 
     def __init__(self, encoder: Encoder, decoder: Decoder, src_embed: InputEmbeddings, tgt_embed: InputEmbeddings, src_pos: PositionalEncoding, tgt_pos: PositionalEncoding, projection_layer: ProjectionLayer) -> None:
@@ -213,7 +223,6 @@ class Transformer(nn.Module):
         # (batch, seq_len, vocab_size)
         return self.projection_layer(x)
     
-
 def build_transformer(src_vocab_size: int, tgt_vocab_size: int, src_seq_len: int, tgt_seq_len: int, d_model: int=512, N: int=6, h: int=8, dropout: float=0.1, d_ff: int=2048) -> Transformer:
     # Create the embedding layers
     src_embed = InputEmbeddings(d_model, src_vocab_size)
